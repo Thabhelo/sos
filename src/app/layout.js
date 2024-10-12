a9225143bfd6d7985c0e0dc15e0fadef78b3e386
tomernahum <git@tomer.nahum.org>
2024-10-12 11:07:37 -0400

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"></link>
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
